827726b72151c17ffdb6f8225f06a2c82086d54f
Ryan SH <ryanhilario5@hotmail.com>
2025-02-14 15:34:04 -0300

    criando: JS para deixa ativo item do Orçamento clicado anteriormente

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// link no header sempre ativo ao estar na página dele
 const links = document.querySelectorAll("header .header-menu a");
 
 function linkAtivo(link) {
@@ -7,3 +8,16 @@ function linkAtivo(link) {
 }
 
 links.forEach(linkAtivo);
+
+// ativar itens do Orçamento clicado anteriormente
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+    console.log(elemento);
+  }
+}
+
+parametros.forEach(ativarProduto);
